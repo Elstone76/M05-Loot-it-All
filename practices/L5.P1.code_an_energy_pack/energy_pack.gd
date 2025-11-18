7a6683ecb6d58f03ab5f0b49b30faf8b94dac59b
Elstone76 <elstone1@s.sfusd.edu>
2025-11-18 13:43:05 -0800

    pasted files over after L6

--- practices/L5.P1.code_an_energy_pack/energy_pack.gd
+++ practices/L5.P1.code_an_energy_pack/energy_pack.gd
@@ -1,9 +1,15 @@
 extends Area2D
+var health := 20
 
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
-
+	# This call updates the health bar to match the health variable when the
+	# game starts.
+	set_health(health)
+	
+func set_health(new_health: int) -> void:
+	health = new_health
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	queue_free()
+	set_health(health + 10)
